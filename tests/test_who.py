012f8299d0155e4f02b6d9dba1cab413d3cf4f90
baamboo <beom5236@gmail.com>
2023-10-19 15:29:34 +0900

    done

--- tests/test_who.py
+++ tests/test_who.py
@@ -1,4 +1,11 @@
-from hello_baamsoo.who import my_name
+from hello_baamsoo.who import my_name, who, plus
 
 def test_my_name():
     my_name()
+
+def test_who():
+    who()
+
+def test_plus():
+    r = plus(1, 3)
+    assert r ==  5
